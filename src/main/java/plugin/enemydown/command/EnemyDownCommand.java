4915019bf0dd1572f04cd87fbd6faa78eaeeb668
yu ji <leon5617@gmail.com>
2023-06-09 06:41:19 +0900

    Random要素の追加

--- src/main/java/plugin/enemydown/command/EnemyDownCommand.java
+++ src/main/java/plugin/enemydown/command/EnemyDownCommand.java
@@ -1,18 +1,58 @@
 package plugin.enemydown.command;
 
+import java.util.List;
+import java.util.SplittableRandom;
+import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 
 public class EnemyDownCommand implements CommandExecutor {
 
   @Override
+  /**Day11 空腹時に回復できる**/
   public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
     if (sender instanceof Player player) {
+      World world = player.getWorld();
       player.setHealth(20);
       player.setFoodLevel(20);
+
+      world.spawnEntity(getEnemySpawnLocation(player, world), getEnemy());
     }
     return false;
+  }
+
+
+  /**
+   * Day12 敵の出現エリアを取得
+   *
+   * @param player 　コマンドを実行したプレイヤー
+   * @param world  　コマンドを実行したプレイヤーが所属するワールド
+   * @return　敵の出現場所
+   */
+
+  private Location getEnemySpawnLocation(Player player, World world) {
+    Location playerlocation = player.getLocation();
+    int randomX = new SplittableRandom().nextInt(20) - 10;
+    int randomZ = new SplittableRandom().nextInt(20) - 10;
+
+    double x = playerlocation.getX() + randomX;
+    double y = playerlocation.getY();
+    double Z = playerlocation.getZ() + randomZ;
+
+    return new Location(world, (x), y, (Z));
+  }
+
+  /**
+   * ランダムで敵を出現させる
+   *
+   * @return　敵
+   */
+  private EntityType getEnemy() {
+    List<EntityType> enemyList = List.of(EntityType.ZOMBIE, EntityType.SKELETON);
+    return enemyList.get(new SplittableRandom().nextInt(2));
   }
 }
