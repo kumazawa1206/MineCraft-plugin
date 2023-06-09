0f2bd2a940ca5ae63d89b43ca83e12bdddedcdcd
yu ji <leon5617@gmail.com>
2023-06-10 00:28:49 +0900

    点数の計算と合計表示

--- src/main/java/plugin/enemydown/EnemyDown.java
+++ src/main/java/plugin/enemydown/EnemyDown.java
@@ -1,16 +1,17 @@
 package plugin.enemydown;
 
 import org.bukkit.Bukkit;
-import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 import plugin.enemydown.command.EnemyDownCommand;
 
-public final class EnemyDown extends JavaPlugin implements Listener {
+public final class EnemyDown extends JavaPlugin {
 
   @Override
   public void onEnable() {
-    Bukkit.getPluginManager().registerEvents(this, this);
-    getCommand("EnemyDown").setExecutor(new EnemyDownCommand());
+    EnemyDownCommand enemyDownCommand = new EnemyDownCommand();
+    Bukkit.getPluginManager().registerEvents(enemyDownCommand, this);
+    getCommand("enemyDown").setExecutor(enemyDownCommand);
   }
+
 
 }
--- src/main/java/plugin/enemydown/command/EnemyDownCommand.java
+++ src/main/java/plugin/enemydown/command/EnemyDownCommand.java
@@ -1,6 +1,8 @@
 package plugin.enemydown.command;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 import java.util.SplittableRandom;
 import org.bukkit.Location;
 import org.bukkit.Material;
@@ -10,15 +12,23 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
 
-public class EnemyDownCommand implements CommandExecutor {
+public class EnemyDownCommand implements CommandExecutor, Listener {
+
+  private Player player;
+  private int score;
+  private Map<Player, Integer> scores;
 
   @Override
   /**空腹時に回復できる**/
   public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
     if (sender instanceof Player player) {
+      this.player = player;
       World world = player.getWorld();
 
       //プレイヤーの状態を初期化(体力と空腹度を最大値にする)
@@ -27,6 +37,22 @@ public class EnemyDownCommand implements CommandExecutor {
       world.spawnEntity(getEnemySpawnLocation(player, world), getEnemy());
     }
     return false;
+  }
+
+  @EventHandler
+  public void onEnemyDeath(EntityDeathEvent e) {
+    Player player = e.getEntity().getKiller();
+    if (Objects.isNull(player)) {
+      return;
+    }
+    if (Objects.isNull(this.player)) {
+      return;
+    }
+
+    if (this.player.getName().equals(player.getName())) {
+      score += 10;
+      player.sendMessage("敵を倒した！現在のスコアは" + score + "点");
+    }
   }
 
   /**
@@ -39,11 +65,11 @@ public class EnemyDownCommand implements CommandExecutor {
     player.setFoodLevel(20);
 
     PlayerInventory inventory = player.getInventory();
-    inventory.setHelmet(new ItemStack(Material.NETHERITE_HELMET));
-    inventory.setChestplate(new ItemStack(Material.NETHERITE_CHESTPLATE));
-    inventory.setLeggings(new ItemStack(Material.NETHERITE_LEGGINGS));
-    inventory.setBoots(new ItemStack(Material.NETHERITE_BOOTS));
-    inventory.setItemInMainHand(new ItemStack(Material.NETHERITE_SWORD));
+    inventory.setHelmet(new ItemStack(Material.DIAMOND_HELMET));
+    inventory.setChestplate(new ItemStack(Material.DIAMOND_CHESTPLATE));
+    inventory.setLeggings(new ItemStack(Material.DIAMOND_LEGGINGS));
+    inventory.setBoots(new ItemStack(Material.DIAMOND_BOOTS));
+    inventory.setItemInMainHand(new ItemStack(Material.DIAMOND_SWORD));
   }
 
 
