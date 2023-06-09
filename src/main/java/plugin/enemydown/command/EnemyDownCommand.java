9b6ab165f79c8b57adfc9f6e85611477f3384cf5
yu ji <leon5617@gmail.com>
2023-06-09 23:13:44 +0900

    装備の変更

--- src/main/java/plugin/enemydown/command/EnemyDownCommand.java
+++ src/main/java/plugin/enemydown/command/EnemyDownCommand.java
@@ -3,31 +3,52 @@ package plugin.enemydown.command;
 import java.util.List;
 import java.util.SplittableRandom;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
 
 public class EnemyDownCommand implements CommandExecutor {
 
   @Override
-  /**Day11 空腹時に回復できる**/
+  /**空腹時に回復できる**/
   public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
     if (sender instanceof Player player) {
       World world = player.getWorld();
-      player.setHealth(20);
-      player.setFoodLevel(20);
+
+      //プレイヤーの状態を初期化(体力と空腹度を最大値にする)
+      initPlayerStatus(player);
 
       world.spawnEntity(getEnemySpawnLocation(player, world), getEnemy());
     }
     return false;
   }
 
+  /**
+   * ゲームを始める前にプレーヤーの状態を設定 体力・空腹度を最大値にし、装備はネザライト一式に変更。
+   *
+   * @param player
+   */
+  private void initPlayerStatus(Player player) {
+    player.setHealth(20);
+    player.setFoodLevel(20);
+
+    PlayerInventory inventory = player.getInventory();
+    inventory.setHelmet(new ItemStack(Material.NETHERITE_HELMET));
+    inventory.setChestplate(new ItemStack(Material.NETHERITE_CHESTPLATE));
+    inventory.setLeggings(new ItemStack(Material.NETHERITE_LEGGINGS));
+    inventory.setBoots(new ItemStack(Material.NETHERITE_BOOTS));
+    inventory.setItemInMainHand(new ItemStack(Material.NETHERITE_SWORD));
+  }
+
 
   /**
-   * Day12 敵の出現エリアを取得
+   * 敵の出現エリアを取得
    *
    * @param player 　コマンドを実行したプレイヤー
    * @param world  　コマンドを実行したプレイヤーが所属するワールド
