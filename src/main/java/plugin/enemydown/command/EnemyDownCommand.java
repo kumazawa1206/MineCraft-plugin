0f2468a2f16a701586710d383487a2e923dfba0d
yu ji <leon5617@gmail.com>
2023-06-19 06:28:22 +0900

    ゲーム終了後にスコアが入らないようにする

--- src/main/java/plugin/enemydown/command/EnemyDownCommand.java
+++ src/main/java/plugin/enemydown/command/EnemyDownCommand.java
@@ -56,22 +56,26 @@ public class EnemyDownCommand extends BaseCommand implements Listener {
   public void onEnemyDeath(EntityDeathEvent e) {
     LivingEntity enemy = e.getEntity();
     Player player = enemy.getKiller();
-    if (Objects.isNull(player) || playerScoreList.isEmpty()) {
+
+    if (Objects.isNull(player) || spanEntityList.stream()
+        .noneMatch(entity -> entity.equals(enemy))) {
       return;
     }
 
-    for (PlayerScore playerScore : playerScoreList) {
-      if (playerScore.getPlayerName().equals(player.getName())) {
-        int point = switch (enemy.getType()) {
-          case ZOMBIE -> 10;
-          case SKELETON, WITCH -> 20;
-          default -> 0;
-        };
+    playerScoreList.stream()
+        .filter(p -> p.getPlayerName().equals(player.getName()))
+        .findFirst()
+        .ifPresent(p -> {
+          int point = switch (enemy.getType()) {
+            case ZOMBIE -> 10;
+            case SKELETON, WITCH -> 20;
+            default -> 0;
+          };
 
-        playerScore.setScore(playerScore.getScore() + point);
-        player.sendMessage("敵を倒した！現在のスコアは" + playerScore.getScore() + "点");
-      }
-    }
+          p.setScore(p.getScore() + point);
+          player.sendMessage("敵を倒した！現在のスコアは" + p.getScore() + "点");
+
+        });
   }
 
   /**
@@ -142,6 +146,7 @@ public class EnemyDownCommand extends BaseCommand implements Listener {
             0, 60, 0);
 
         spanEntityList.forEach(Entity::remove);
+        spanEntityList = new ArrayList<>();
         return;
       }
       Entity spawnEntity = player.getWorld().spawnEntity(getEnemySpawnLocation(player), getEnemy());
